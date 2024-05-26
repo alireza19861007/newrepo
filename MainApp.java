d1d14832dda36ba8cf3287f156cd379e3c36e8e8
MSI <alirezaehterami13@gmail.com>
2024-05-26 16:18:15 +0330

    commit dovom

--- MainApp.java
+++ MainApp.java
@@ -1,5 +1,18 @@
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD!");
+
+
+        float f1 = 12.3f;
+        float f2 = 12;
+        System.out.println(f1);
+        System.out.println(f2);
+        double d1 = 123123;
+        double d2 = 124123.1231;
+        double d3 = 4765757.123333;
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+
+
     }
-}
+    }
